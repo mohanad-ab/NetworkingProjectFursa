93260ff9174f04b19ff1e60692b32a59396a6d76
mohanad <mohanadabuhamad78@gmail.com>
2024-05-30 01:31:49 +0300

    v2

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -14,16 +14,15 @@ ssh-keygen -t rsa -b 2048 -f ~/new_key -q -N ""
 # Copy the public key to the private instance
 scp -i ~/key.pem ~/new_key.pub ubuntu@$PRIVATE_INSTANCE_IP:~/
 
-# Override the new public key to the authorized_keys file on the private instance
-ssh -i ~/key.pem ubuntu@$PRIVATE_INSTANCE_IP "cat ~/new_key.pub > ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+# Override the new public key to the authozed_keys file on the private instance
+ssh -i ~/key.pem ubuntu@$PRIVATE_INSTANCE_IP "cat ~/new_key.pub > ~/.ssh/authorized_keys"
 
-# Update the private key file on the local machine
 cat ~/new_key > ~/key.pem
 
-# Remove the temporary files
-rm -f ~/new_key ~/new_key.pub
+rm ~/new_key
+
+rm ~/new_key.pub
 
 # Remove the new public key file from the public instance
-ssh -i ~/key.pem ubuntu@$PRIVATE_INSTANCE_IP "rm -f ~/new_key.pub"
 
-echo "SSH keys have been rotated successfully."
+#echo "SSH key rotation attempted. Please verify the connection manually."
